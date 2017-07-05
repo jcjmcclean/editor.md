36b1ffd20009ba74ae84e21f18a71077bf186cbc
James McClean <jcjmcclean@gmail.com>
2017-07-05 21:08:53 +0100

    Attempting to add extraKey as per cm example
    Hacky :)

--- src/editormd.js
+++ src/editormd.js
@@ -668,6 +668,55 @@
          * 
          * @returns {editormd}  返回editormd的实例对象
          */
+	    var dummy = {
+        attrs: {
+          color: ["red", "green", "blue", "purple", "white", "black", "yellow"],
+          size: ["large", "medium", "small"],
+          description: null
+        },
+        children: []
+      };
+  
+  //HERE
+
+      var tags = {
+        "!top": ["top"],
+        "!attrs": {
+          id: null,
+          class: ["A", "B", "C"]
+        },
+        top: {
+          attrs: {
+            lang: ["en", "de", "fr", "nl"],
+            freeform: null
+          },
+          children: ["animal", "plant"]
+        },
+        animal: {
+          attrs: {
+            name: null,
+            isduck: ["yes", "no"]
+          },
+          children: ["wings", "feet", "body", "head", "tail"]
+        },
+        plant: {
+          attrs: {name: null},
+          children: ["leaves", "stem", "flowers"]
+        },
+        wings: dummy, feet: dummy, body: dummy, head: dummy, tail: dummy,
+        leaves: dummy, stem: dummy, flowers: dummy
+      };
+
+      function completeAfter(cm, pred) {
+        var cur = cm.getCursor();
+        if (!pred || pred()) setTimeout(function() {
+          if (!cm.state.completionActive)
+            cm.showHint({completeSingle: false});
+        }, 100);
+        return CodeMirror.Pass;
+      }
+      
+        //HERE
         
         setCodeMirror : function() { 
             var settings         = this.settings;
@@ -692,7 +741,8 @@
                 extraKeys                 : {
                                                 "Ctrl-Q": function(cm) { 
                                                     cm.foldCode(cm.getCursor()); 
-                                                }
+                                                },
+						"'<'": completeAfter,  //HERE
                                             },
                 foldGutter                : settings.codeFold,
                 gutters                   : ["CodeMirror-linenumbers", "CodeMirror-foldgutter"],
